3b99900e7bd2dae5f1d12a6ab073d4fdfccc2d85
Tanmoy Debnath <tanmoydebnath2204045@gmail.com>
2025-04-06 19:16:14 +0600

    Adding project

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -17,8 +17,9 @@ export default function Projects() {
           <p>SystemSage's Smart Aquaculture System is an IoT-powered solution combining hardware and software to monitor and automate fish farming operations. Using ESP32, sensors, and Wi-Fi/cellular connectivity, it enables real-time water quality monitoring, automated feeding, and remote management, ensuring optimal conditions for sustainable aquaculture.</p>
         </div>
         <div className="project-container-one">
-          <h3>Formula Racing Car</h3>
-          <p>phihoihroihaoihgaiohgohoi oiahoithgoiaho hoiah oahorht oih hioh oihaoh tsihoihsoih   oihohoiahrgiohsiohgiohfiogh  hiohiohgoiishoihgsoi iohgohsiohboinzn;ngp;snonshioh ihoiahoirhgoshohgboishboiorhtio gshoighosih hoiehgdoihs sgidhoi hoi hiohoihoihgo hoihioho hgoshfidhgiosh hoihgoih oihsh oihso hgoih soh hgiohsiogh iohfdinblanzo hgoh</p>
+          <h3>Mechanical Design Engineer – Team Crack Platoon (Formula Student Team)</h3>
+          <p>
+          Contributed to the research, design, and fabrication of high-performance student formula race cars as part of Bangladesh’s first Formula SAE team. Played a key role in the mechanical design process, focusing on vehicle dynamics, structural components, and CAD modeling. Collaborated with multidisciplinary teams to develop and test components for international competition, including Formula Bharat 2024, where the team proudly represented Bangladesh on a global motorsport engineering stage.</p>
         </div>
       </div>
     </div>
